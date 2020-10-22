11c535c36f740312890cd3d64a111074f2560b78
Avani1994 <avaninith@gmail.com>
2020-10-21 22:22:59 -0600

    adding single javascript file as entry point

---
+++ src/index.js
@@ -0,0 +1,8 @@
+
+// Testing code for JS
+function component() {
+    const element = document.createElement("div");
+    element.innerHTML = "Hello world";
+    return element;
+}
+document.body.appendChild(component());
