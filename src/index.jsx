aed3607a119786c6a02a6715c586d5198d77c578
Avani1994 <avaninith@gmail.com>
2020-10-22 00:05:36 -0600

    added support for .scss files along with unique hashcode generation

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import './scss/styles.scss';
 
 // Testing code for JS
 function component() {
@@ -14,7 +15,7 @@ document.body.appendChild(component());
 class App extends React.Component{
     render() {
         return(
-            <div style={{ display: "flex" }}>
+            <div className="title" style={{ display: "flex" }}>
                 <strong>Hello React</strong>
             </div>
         );
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,8 @@
 // Webpack uses this to work with directories
 const path = require('path');
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
 
 // This is the main configuration object.
 // Here you write different options and tell Webpack what to do
@@ -33,12 +35,42 @@ module.exports = {
             presets: ['@babel/preset-env', "@babel/preset-react"]
           }
         }
+    },
+    {
+        // Apply rule for .sass, .scss or .css files
+        test: /\.(sa|sc|c)ss$/,
+  
+        // Set loaders to transform files.
+        // Loaders are applying from right to left(!)
+        // The first loader will be applied after others
+        use: [
+               {
+                // After all CSS loaders we use plugin to do his work.
+                // It gets all transformed CSS and extracts it into separate
+                // single bundled file
+                loader: MiniCssExtractPlugin.loader
+               },
+               {
+                 // This loader resolves url() and @imports inside CSS
+                 loader: "css-loader",
+               },
+               {
+                 // First we transform SASS to standard CSS
+                 loader: "sass-loader",
+                 options: {
+                   implementation: require("sass")
+                 }
+               }
+             ]
       }
   ]
   },
   plugins: [
     new HtmlWebpackPlugin({
       template: "./src/index.html",
+    }),
+    new MiniCssExtractPlugin({
+        filename: "[name].[contenthash].css",
     }),
   ],
 };
